c01f42c3ccb84a683e81bc237961c7caa2d17be9
Somesh <somesh.bhaskar@webdunia.net>
2020-11-18 00:19:10 +0530

    updated css

--- src/app/project/dog-breed-classifier/dog-breed-classifier.component.ts
+++ src/app/project/dog-breed-classifier/dog-breed-classifier.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dog-breed-classifier',
   templateUrl: './dog-breed-classifier.component.html',
-  styleUrls: ['./dog-breed-classifier.component.scss']
+  styleUrls: ['./dog-breed-classifier.component.scss'],
 })
 export class DogBreedClassifierComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  url: any = 'assets/images/select-image.png';
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => {
+        // called once readAsDataURL is completed
+        this.url = event.target.result;
+      };
+    }
   }
-
 }
